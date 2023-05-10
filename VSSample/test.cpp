a5a37d1a42f571b67d285931affe53116c1324ee
Andy Lang <djdreviz@gmail.com>
2023-05-10 22:38:18 +0300

    Hi cin test

--- VSSample/test.cpp
+++ VSSample/test.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Welcome to the C++ world.";
+    string name;
+
+    cout << "Welcome to the C++ world." << endl;
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Hi " << name << "!" << endl;
     return 0;
 }
