bf2409587ae99abfc07bfe93e1b4929d7c6efc65
Andy Lang <djdreviz@gmail.com>
2023-05-11 00:40:19 +0300

    Input and Output in C++

--- VSSample/test.cpp
+++ VSSample/test.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-using namespace std;
-
 int main()
 {
-    string name;
-
-    cout << "Welcome to the C++ world." << endl;
-    cout << "What is your name? ";
-    cin >> name;
-    cout << "Hi " << name << "!" << endl;
-    return 0;
+    std::cout << "Welcome to C++ World!:";
+   return 0;
 }
