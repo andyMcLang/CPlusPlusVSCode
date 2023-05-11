245aeb91fa7e39ec09a68f94524280d45e59f9fa
Andy Lang <djdreviz@gmail.com>
2023-05-12 01:15:48 +0300

    namespaces in C++

---
+++ VSSample/ns.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Enter your numbers:" << endl;
+    int n1 = 0;
+    cin >> n1;
+    cout << "The entered number is " << n1;
+    return 0;
+}
