46253e3d91770b891058fdb043dfc5f4abc133f4
Andy Lang <djdreviz@gmail.com>
2023-05-17 01:17:23 +0300

    Literals in C++

---
+++ VSSample/hiekkalaatikko.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool b = 35;
+    cout << b << endl;
+
+    int i = b;
+    cout << i << endl;
+
+    i = 3.14;
+    cout << i << endl;
+
+    double pi = i;
+    cout << pi << endl;
+
+    unsigned char c = -1;
+    cout << c << endl;
+
+    return 0;
+}
